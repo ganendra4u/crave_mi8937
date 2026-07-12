5253c53805f0deb0c72cf06e0958e8d017eb33e9
Ｇａｎｘ <daffaofficial224@gmail.com>
2026-07-12 11:27:17 +0700

    Update device.mk path and lunch command

--- avium.sh
+++ avium.sh
@@ -12,7 +12,7 @@ export BUILD_USERNAME=ganx0000
 export BUILD_HOSTNAME=crave
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 
-cat >> device/vendor_mi8937_4_19/mi8937_4_19/device.mk << 'EOF'
+cat >> device/vendor_Mi8937/Mi8937/device.mk << 'EOF'
 AVIUM_MAINTAINER := ganx0000
 AVIUM_SETTINGS_SOC_MODEL_NAME := Snapdragon 430 Ultra
 AVIUM_SETTINGS_DEVICE_CODENAME := MI8937
@@ -20,6 +20,6 @@ EOF
 
 source build/envsetup.sh
 
-lunch lineage_mi8937_4_19-bp4a-userdebug
+lunch lineage_Mi8937_4_19-bp4a-userdebug
 
 m bacon
