263d5dde24937cb334ca2a5ab609606343255079
GanX <daffaofficial224@gmail.com>
2026-08-25 18:57:34 +0700

    Update avium.sh

--- avium.sh
+++ avium.sh
@@ -4,6 +4,7 @@ rm -rf .repo/local_manifests/
 
 repo init -u https://github.com/AviumUI/android_manifests -b avium-16.2 --git-lfs --depth=1
 
+mkdir -p .repo/local_manifests
 curl -L -o .repo/local_manifests/lineage-23.2.xml https://raw.githubusercontent.com/Rve27/local_manifests/master/lineage-23.2.xml
 
 /opt/crave/resync.sh
@@ -12,9 +13,10 @@ export BUILD_USERNAME=Ganendra1945
 export BUILD_HOSTNAME=crave
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 
-cat >> device/vendor_Mi8937/Mi8937/device.mk << 'EOF'
+cat >> device/xiaomi/Mi8937/lineage_Mi8937_4_19.mk << 'EOF'
+
 AVIUM_MAINTAINER := Ganendra1945
-AVIUM_SETTINGS_SOC_MODEL_NAME := Snapdragon 425 Ultra
+AVIUM_SETTINGS_SOC_MODEL_NAME := MSM8937
 AVIUM_SETTINGS_DEVICE_CODENAME := MI8937
 EOF
 
